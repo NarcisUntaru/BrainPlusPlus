14bda657bdba6f1e06795d920140b983c97fe81e
NarcisUntaru <narcis.alexandru02@gmail.com>
2025-11-13 14:59:38 +0200

    Add C++ code compilation endpoint to HomeController.

--- Brain++/Controllers/HomeController.cs
+++ Brain++/Controllers/HomeController.cs
@@ -1,21 +1,34 @@
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Brain__.Models;
+using Brain__.Services;
+using Brain__.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Brain__.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICppCompilerService _cppCompiler;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICppCompilerService cppCompiler)
         {
             _logger = logger;
+            _cppCompiler = cppCompiler;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CompileCpp([FromBody] CppCodeRequest request)
+        {
+            var result = await _cppCompiler.CompileAndRunAsync(request.Code, request.Input);
+            return Json(result);
         }
 
         public IActionResult Privacy()
@@ -28,5 +41,11 @@ namespace Brain__.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+    }
+
+    public class CppCodeRequest
+    {
+        public string Code { get; set; } = string.Empty;
+        public string Input { get; set; } = string.Empty;
     }
 }
