5c79490c56984ceead56bacc169295543e94e0d0
NarcisUntaru <narcis.alexandru02@gmail.com>
2025-12-03 01:52:32 +0200

    Refactor middleware and add Swagger support

--- BrainPlusPlus.Server/Program.cs
+++ BrainPlusPlus.Server/Program.cs
@@ -1,28 +1,31 @@
+using BrainPlusPlus.Server.Services;
+using BrainPlusPlus.Server.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register ProcessExecutor service
+builder.Services.AddScoped<IProcessService, ProcessService>();
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
-app.MapStaticAssets();
+app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.MapFallbackToFile("/index.html");
 
 app.Run();
